b358fb8c80f042fc55002fb15f47327949b883d6
davie500 <davichelberger@gmail.com>
2025-09-12 21:57:42 -0300

    arrumado uma coisa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from "express";
 import { userRoutes } from "./routes/user.routes.js";
 import { loginRoutes } from "./routes/login.routes.js";
-import { postRoutes } from "./routes/pesquisa.routes.js";
+import { pesquisaRoutes } from "./routes/pesquisa.routes.js";
 import cors from "cors"
 import { perguntasRoutes } from "./routes/perguntas.routes.js";
 const app = express()
@@ -13,7 +13,7 @@ app.use(express.static('paginas'));
 app.use(express.json())
 app.use("/user",userRoutes)
 app.use("/login",loginRoutes)
-app.use("/pesquisa",postRoutes)
+app.use("/pesquisa",pesquisaRoutes)
 app.use("/perguntas",perguntasRoutes)
 
 export default app
